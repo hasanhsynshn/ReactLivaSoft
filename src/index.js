57929b2785cc840c3c07a58685274fbefd05229f
hasanhsynshn <49681419+hasanhsynshn@users.noreply.github.com>
2023-04-08 15:11:47 +0300

    Update index.js
    Soru çözümü

--- src/index.js
+++ src/index.js
@@ -10,13 +10,18 @@ class App extends React.Component {
      * Aşağıdaki mantık hatasını açıklayınız.
      * input a girilen değeri 250ms sonra ekrana yazdırınız
      */
-
+ /**
+     * Cevap; Bu kodla ilgili sorun, setTimeout işlevinde ayarlanan gecikme süresinin (50ms) çok kısa olmasıdır,
+     *  bu da setState işlevinin çok sık çağrılmasına neden olarak bileşenin gereksiz yere yeniden oluşturulmasına ve performansın düşmesine neden olur.
+     *  ** Asıl sorun ise kullanıcının inputa giriş yaparken yeterli süresinin olmamasıdır. Dolayısıyla bu senaryoda
+     *  settimeout işlevi için 250 ms vermek daha doğru olacaktır. Çünkü girilen değeri setstate ile set ederken kullanıcıya 50 ms verirsek istediği değeri girmesi için yeterli süreye sahip olmaz.
+     */
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
       this.setState({
         search: event.target.value
       });
-    }, 50);
+    }, 250);
   };
 
   render() {
